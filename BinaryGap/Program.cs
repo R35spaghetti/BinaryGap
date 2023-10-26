8025eb605265c538bb633f7db6d2a4436ed2d859
rb <rkrb@tyfonmail.se>
2023-10-27 00:06:38 +0200

    converts decimal to binary

---
+++ BinaryGap/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices.JavaScript;
+using BinaryGap;
+
+Solution test = new Solution();
+
+Console.WriteLine(test.SolutionMethod(1041));
+
+namespace BinaryGap
+{
+    public class Solution
+    {
+        public long SolutionMethod(int n)
+        {
+            long remainder = 0;
+            string binary = "";
+            do
+            {
+                
+                if (n % 2 == 0)
+                {
+                    n = n / 2;
+                    
+                    binary += "0";
+                }
+                else
+                {
+                    n = n / 2;
+
+                    binary += "1";
+                }
+
+                remainder = n;
+            } while (remainder != 0);
+
+            return Convert.ToInt64(binary);
+        }
+    }
+}
+
+
+//
+// A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+//
+//     For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3.
+// The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+//
+//     Write a function:
+//
+// class Solution { public int solution(int N); }
+//
+// that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+//
+//     For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+//
+//     Write an efficient algorithm for the following assumptions:
+//
+// N is an integer within the range [1..2,147,483,647].
+//
